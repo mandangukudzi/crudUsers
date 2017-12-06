842e9b4b6c0b3ce654071df0db899e0ad128aae3
Mandangu Kudzanayi <mandangu@kudzi@gmail.com>
2017-12-06 09:42:22 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+//angular firestore imports...
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+const firestoreconfig = {
+  apiKey: "AIzaSyDQoxxu0Gt7Y_HpCwI5a0L6Yqi4HsRuprM",
+  authDomain: "propaycrudusers.firebaseapp.com",
+  databaseURL: "https://propaycrudusers.firebaseio.com",
+  projectId: "propaycrudusers",
+  storageBucket: "propaycrudusers.appspot.com",
+  messagingSenderId: "516282574422"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firestoreconfig),
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
